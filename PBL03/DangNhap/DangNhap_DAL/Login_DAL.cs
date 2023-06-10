bcad90581f455d3b2cece3c544a7e5294a7251d6
Minh13112003 <118954186+Minh13112003@users.noreply.github.com>
2023-06-10 22:51:22 +0700

    Add files via upload

--- PBL03/DangNhap/DangNhap_DAL/Login_DAL.cs
+++ PBL03/DangNhap/DangNhap_DAL/Login_DAL.cs
@@ -1,4 +1,4 @@
-﻿using PBL03.ThuNgan.ThuNgan_VIEW;
+﻿using Org.BouncyCastle.Crypto.Generators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +10,7 @@ namespace PBL03.DangNhap.DangNhap_DAL
 {
     internal class Login_DAL
     {
-        private PBL03Entities db;
+        private PBL3Entities1 db;
         private static Login_DAL _Instance;
         public static Login_DAL Instance
         {
@@ -24,89 +24,45 @@ namespace PBL03.DangNhap.DangNhap_DAL
 
         private Login_DAL()
         {
-            db = new PBL03Entities();
+            db = new PBL3Entities1();
         }
-        public void dangnhap(string Username,string Password)
+        
+
+        public bool CheckQuanLy(string UserName,string PassWord)
         {
-            /*using (var context = new PBL03Entities())
-            {
-                var user1 = context.Accounts.FirstOrDefault(u => u.Username == Username && u.NameType == "quanly");
-                var uses2 = context.Accounts.FirstOrDefault(u => u.Username == Username && u.NameType == "thungan");
-                if (user1 == null && uses2 == null)
-                {
-                    MessageBox.Show("Bạn đã nhập sai tài khoản hoặc mật khẩu");
-                }
-                else
-                {
-                    string hashedPassword = user1 != null ? user1.PW : uses2.PW;
-                    if (BCrypt.Net.BCrypt.Verify(Password, hashedPassword))
-                    {
-
-                        if (user1 != null)
-                        {
-                            Form_Admin fad = new Form_Admin();
-                            fad.Show();
-                        }
-                        else
-                        {
-                            FormMain ftn = new FormMain();
-                            ftn.Show();
-                        }
-                    }
-
-                    else
-                    {
-                        MessageBox.Show("Bạn đã nhập sai tài khoản hoặc mật khẩu");
-                    }
-                }
-            }*/
-
-                var user1 = db.Accounts.FirstOrDefault(u => u.Username == Username && u.NameType == "quanly");
-                var uses2 = db.Accounts.FirstOrDefault(u => u.Username == Username && u.NameType == "thungan");
-                if (user1 == null && uses2 == null)
-                {
-                    MessageBox.Show("Bạn đã nhập sai tài khoản hoặc mật khẩu");
-                }
-                else
-                {
-                    string hashedPassword = user1 != null ? user1.PW : uses2.PW;
-                    if (BCrypt.Net.BCrypt.Verify(Password, hashedPassword))
-                    {
-
-                        if (user1 != null)
-                        {
-                            Form_Admin fad = new Form_Admin();
-                            fad.Show();
-                        }
-                        else
-                        {
-                            FormMain ftn = new FormMain();
-                            ftn.lbNameUser.Text = Username;
-                            ftn.Show();
-                        }
-                    }
-
-                    else
-                    {
-                        MessageBox.Show("Bạn đã nhập sai tài khoản hoặc mật khẩu");
-                    }
+            var user1 = db.Accounts.FirstOrDefault(u => u.Username.Equals(UserName) && u.NameType == "quanly");
+            if (user1 == null) return false;
+            else {
+                if (BCrypt.Net.BCrypt.Verify(PassWord, user1.PW) == true) return true;
+                else return false;
             }
+            
         }
+
+        public bool CheckThuNgan(string UserName, string PassWord)
+        {
+            var user1 = db.Accounts.FirstOrDefault(u => u.Username.Equals(UserName) && u.NameType == "thungan");
+            if (user1 == null) return false;
+            else
+            {
+                if (BCrypt.Net.BCrypt.Verify(PassWord, user1.PW) == true) return true;
+                else return false;
+            }
+
+        }
+
         public bool CheckInfor(string user, string name, string phonenumber, string address)
         {
             var query = db.Employees.FirstOrDefault(u => u.Acc == user && u.Name_Employee == name && u.PhoneNumber == phonenumber && u.Address_Employee == address);
             if (query == null) return false;
             else return true;
         }
-            
-        public void ChangePass(string Account,string newpass)
+
+        public void ChangePass(string Account, string newpass)
         {
             var query = db.Accounts.FirstOrDefault(u => u.Username == Account);
             query.PW = newpass;
             db.SaveChanges();
         }
-    
     }
-
-   
 }
